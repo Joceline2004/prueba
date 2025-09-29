21dfff5858db620480dfb7f84fa6489b89131ae3
Joceline <joceline.delatorre6785@alumnos.udg.mx>
2025-09-29 11:00:05 -0600

    prueba

---
+++ 01_print.py
@@ -0,0 +1,20 @@
+print("hola")
+print("mundo")
+#comentario una linea
+"""
+este es 
+un 
+comentario
+de varias lineas
+"""
+print (type(3 ))
+#casting de tipos
+# como cambiar un tipo de valor de a otro
+print("conversion de tipos")
+print(2 + int("100"))
+print("100"+ str(2))
+print(type(float("3.1416")))
+print(int(3.1416))
+
+
+
